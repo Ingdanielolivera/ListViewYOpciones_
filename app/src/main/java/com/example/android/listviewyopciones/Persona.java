5a86a998bd6a773e812056b5f14b55de5661e6cc
Daniel <Daniel@olivera.com>
2017-05-12 21:22:38 -0500

    personas

--- app/src/main/java/com/example/android/listviewyopciones/Persona.java
+++ app/src/main/java/com/example/android/listviewyopciones/Persona.java
@@ -5,14 +5,37 @@ package com.example.android.listviewyopciones;
  */
 
 public class Persona {
+    private String foto;
+    private String cedula;
     private String nombre;
+    private String apellido;
     private String sexo;
     private String pasatiempos;
 
-    public Persona(String nombre, String sexo, String pasatiempos) {
+
+    public Persona(String foto, String cedula, String nombre, String apellido, String sexo, String pasatiempos) {
+        this.foto = foto;
+        this.cedula = cedula;
         this.nombre = nombre;
+        this.apellido = apellido;
         this.sexo = sexo;
         this.pasatiempos = pasatiempos;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
     }
 
     public String getNombre() {
@@ -21,6 +44,14 @@ public class Persona {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
     }
 
     public String getSexo() {
